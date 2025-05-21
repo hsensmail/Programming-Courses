eda20ddeb3f86c63aee3cb4645b7112dcc00baa0
Hussein Ismail <hussein.ismail.230@gmail.com>
2025-05-21 06:38:25 +0300

    Add files via upload

---
+++ Java Script Course/01. Beginners/02. Primitives/01 Variables/live coding/app.js
@@ -0,0 +1,7 @@
+// (reserved keyword): (variable name): (type of value)
+
+let variable; // undefined
+let name = "Hussein Ismail";
+console.log(name);
+const channelName = "Hussein's Academy";
+console.log(channelName);
